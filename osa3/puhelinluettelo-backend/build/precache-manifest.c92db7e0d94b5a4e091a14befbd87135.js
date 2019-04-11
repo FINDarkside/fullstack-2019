d60466628ac2dc6c435750bf46171065e0f2f6c4
Jesse Niininen <findarkside@gmail.com>
2019-04-12 01:11:43 +0300

    Add frontend build

---
+++ osa3/puhelinluettelo-backend/build/precache-manifest.c92db7e0d94b5a4e091a14befbd87135.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddf0684a2d476793d51e",
+    "url": "/static/css/main.b3588452.chunk.css"
+  },
+  {
+    "revision": "ddf0684a2d476793d51e",
+    "url": "/static/js/main.abd85b13.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1c9bdb85615345cb2f60",
+    "url": "/static/js/2.fd585594.chunk.js"
+  },
+  {
+    "revision": "485bbec43cadaba3fc427e349b0087c9",
+    "url": "/index.html"
+  }
+];
