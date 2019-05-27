ab6c5ce49af02bc69d36923b602174f97f95716e
Jesse Niininen <findarkside@gmail.com>
2019-05-27 19:05:07 +0300

    3.16

--- osa3/puhelinluettelo-backend/src/index.js
+++ osa3/puhelinluettelo-backend/src/index.js
@@ -55,6 +55,20 @@ app.post('/api/persons', async (req, res) => {
     res.json(person.toJSON());
 })
 
+app.use((req, res) => {
+    res.status(404).send({ error: 'unknown endpoint' })
+})
+
+app.use((error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError' && error.kind == 'ObjectId') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+})
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
