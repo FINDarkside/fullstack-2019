62ed5df2aa3f07de806f8837e098eeba3df1b90a
FINDarkside <findarkside@gmail.com>
2019-10-05 20:18:15 +0300

    Add Procfile and express.static to serve index.html

--- osa7/backend/src/app.js
+++ osa7/backend/src/app.js
@@ -10,8 +10,10 @@ const loginRouter = require('./controllers/login');
 const middleware = require('./utils/middleware');
 
 console.log(config.MONGODB_URI);
+
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true });
 
+app.use(express.static('build'));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(middleware.getTokenFromRequest);
@@ -19,5 +21,11 @@ app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
 app.use(middleware.errorHandler);
+const path = require('path')
+app.use(function (req, res) {
+  console.log(__dirname);
+  res.sendFile(path.dirname(__dirname) +  '/build/index.html');
+});
+
 
 module.exports = app;
