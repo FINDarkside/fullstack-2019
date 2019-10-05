e753ee2106e4c94f7e82540955fc2f99b4e91cc7
FINDarkside <findarkside@gmail.com>
2019-10-05 19:41:57 +0300

    Add webpack config

--- osa7/frontend/src/services/login.js
+++ osa7/frontend/src/services/login.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = '/api/login';
+const baseUrl = BACKEND_URL + '/api/login';
 
 async function login(username, password) {
   console.log('Login ', username, password);
